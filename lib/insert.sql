91336b45e86754bce1859c9ee8802807f270169e
Motoaki Sumi <msumi@mba2019.hbs.edu>
2019-09-09 03:01:02 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,65 @@
+INSERT INTO users (name) VALUES ('User 1');
+INSERT INTO users (name) VALUES ('User 2');
+INSERT INTO users (name) VALUES ('User 3');
+INSERT INTO users (name) VALUES ('User 4');
+INSERT INTO users (name) VALUES ('User 5');
+INSERT INTO users (name) VALUES ('User 6');
+INSERT INTO users (name) VALUES ('User 7');
+INSERT INTO users (name) VALUES ('User 8');
+INSERT INTO users (name) VALUES ('User 9');
+INSERT INTO users (name) VALUES ('User 10');
+INSERT INTO users (name) VALUES ('User 11');
+INSERT INTO users (name) VALUES ('User 12');
+INSERT INTO users (name) VALUES ('User 13');
+INSERT INTO users (name) VALUES ('User 14');
+INSERT INTO users (name) VALUES ('User 15');
+INSERT INTO users (name) VALUES ('User 16');
+INSERT INTO users (name) VALUES ('User 17');
+INSERT INTO users (name) VALUES ('User 18');
+INSERT INTO users (name) VALUES ('User 19');
+INSERT INTO users (name) VALUES ('User 20');
+
+INSERT INTO projects (title) VALUES
+('title 1'),
+('title 2'),
+('title 3'),
+('title 4'),
+('title 5'),
+('title 6'),
+('title 7'),
+('title 8'),
+('title 9'),
+('title 10');
+
+
+INSERT INTO pledges (amount) VALUES
+(1),
+(2),
+(3),
+(4),
+(5),
+(6),
+(7),
+(8),
+(9),
+(10),
+(11),
+(12),
+(13),
+(14),
+(15),
+(16),
+(17),
+(18),
+(19),
+(20),
+(21),
+(22),
+(23),
+(24),
+(25),
+(26),
+(27),
+(28),
+(29),
+(30);
