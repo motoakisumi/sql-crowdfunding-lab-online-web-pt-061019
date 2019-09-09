00e92d0bd911a38ff479a16a7ee2eca4e59faba8
Motoaki Sumi <msumi@mba2019.hbs.edu>
2019-09-09 03:04:15 +0000

    Done.

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -28,12 +28,12 @@ end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
   "
-  SELECT projects.title, SUM(pledges.amount) - projects.funding_goal
+  SELECT projects.title, SUM(pledges.amount) - projects.funding_goal AS amount_over_goal
   FROM projects
   JOIN pledges
   ON projects.id = pledges.project_id
-  GROUP BY projects.id
-  HAVING SUM(pledges.amount) > projects.funding_goal
+  GROUP BY projects.title
+  HAVING amount_over_goal > 0
   "
 end
 
