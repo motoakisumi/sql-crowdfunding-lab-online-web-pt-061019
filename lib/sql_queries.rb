c1b08ef83c1e54f19472fdf076ec3e40bc5bd4dd
Motoaki Sumi <msumi@mba2019.hbs.edu>
2019-09-09 03:11:23 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -39,7 +39,7 @@ end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_summed_amount
   "
-  SELECTS users.name, SUM(pledges.amount) as sum_amount
+  SELECT users.name, SUM(pledges.amount) as sum_amount
   FROM users
   INNER JOIN pledges
   ON user.id = pledges.user_id
